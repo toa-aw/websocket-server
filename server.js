4b3dcb986bf9ab2975d2eb0d8c7b71fbea188fbb
Tomas Avila <2161611@my.ipleiria.pt>
2019-01-07 22:47:36 +0000

    server

---
+++ server.js
@@ -0,0 +1,60 @@
+/*jshint esversion: 6 */
+
+var app = require('http').createServer();
+
+// Se tiverem problemas com "same-origin policy" deverão activar o CORS.
+
+// Aqui, temos um exemplo de código que ativa o CORS (alterar o url base) 
+
+// var app = require('http').createServer(function(req,res){
+// Set CORS headers
+//  res.setHeader('Access-Control-Allow-Origin', 'http://---your-base-url---');
+//  res.setHeader('Access-Control-Request-Method', '*');
+//  res.setHeader('Access-Control-Allow-Methods', 'UPGRADE, OPTIONS, GET');
+//  res.setHeader('Access-Control-Allow-Credentials', true);
+//  res.setHeader('Access-Control-Allow-Headers', req.header.origin);
+//  if ( req.method === 'OPTIONS' || req.method === 'UPGRADE' ) {
+//      res.writeHead(200);
+//      res.end();
+//      return;
+//  }
+// });
+
+// NOTA: A solução correta depende da configuração do próprio servidor, 
+// e alguns casos do próprio browser.
+// Assim sendo, não se garante que a solução anterior funcione.
+// Caso não funcione é necessário procurar/investigar soluções alternativas
+
+var io = require('socket.io')(app);
+
+var LoggedUsers = require('./loggedusers.js');
+
+app.listen(8080, function(){
+    console.log('listening on *:8080');
+});
+
+// ------------------------
+// Estrutura dados - server
+// ------------------------
+
+// loggedUsers = the list (map) of logged users. 
+// Each list element has the information about the user and the socket id
+// Check loggedusers.js file
+
+let loggedUsers = new LoggedUsers();
+
+io.on('connection', function (socket) {
+
+     socket.on('update_orders', function (order) {
+         socket.broadcast.emit('update_orders', order);
+     });
+
+    //  socket.on('update_meals', function (meal) {
+    //     io.sockets.emit('update_meals', order);
+    // });
+
+    socket.on('update_invoices', function () {
+        io.sockets.emit('update_invoices');
+    });
+
+});
